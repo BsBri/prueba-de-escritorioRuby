d1fc932f07a29666a0b441e6282d7aa100c3b3c5
BsBri <brayanbriones2004@gmail.com>
2025-11-12 16:00:21 -0500

    Add files via upload

---
+++ algoritmos/algoritmo3J.rb
@@ -0,0 +1,32 @@
+
+5+4
+A
+#Prueba Comentarios
+
+#Comentario
+
+#Delimitadores
+
+=begin
+x=5
+y=10; z=x+y
+
+parentesis=(1+x)
+puts "La suma de #{x} y #{y} es #{z}"
+
+persona = { nombre: "Juan", edad: 20}
+puts persona[:nombre]
+
+arreglo = [10, 20, 30]
+
+do
+end
+#Error a propósito
+°
+
+=begin
+Este es un comentario
+de bloque
+=end
+
+
